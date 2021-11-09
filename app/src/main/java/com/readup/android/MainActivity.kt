fbc26ff4fab034e1e7c0268b69ed07e3b9f3d604
Jeff <jeff@jeffcamera.com>
2021-11-09 14:42:53 -0500

    added main web app webview

--- app/src/main/java/com/readup/android/MainActivity.kt
+++ app/src/main/java/com/readup/android/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.readup.android
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainWebView = findViewById<WebView>(R.id.mainWebView)
+
+        mainWebView.settings.domStorageEnabled = true
+        mainWebView.settings.javaScriptEnabled = true
+
+        mainWebView.loadUrl("https://dev.readup.com/?clientType=App&appPlatform=Android&appVersion=1.0.0")
     }
 }
